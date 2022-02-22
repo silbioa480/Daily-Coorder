cda0f143fd7bc460a2fc7a7e9f20b52d5885b996
silbioa480 <silbioa480@gmail.com>
2022-02-22 10:14:11 +0900

    project init

---
+++ src/Router.tsx
@@ -0,0 +1,5 @@
+function Router() {
+  return <div></div>;
+}
+
+export default Router;
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#FCF8FF",
+  textColor: "#130629",
+  accentColor: "#F9F871",
+};
