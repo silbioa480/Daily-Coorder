c1a31925f0074038162543a797b2c665a5a63330
taeyoung <stylight@naver.com>
2022-02-23 19:46:21 +0900

    태영: 마이페이지 사이드메뉴바 css 수정

--- src/css/MyPage_SideMenubarCss.tsx
+++ src/css/MyPage_SideMenubarCss.tsx
@@ -1,8 +1,23 @@
-import styled from "styled-components";
-import MyPage_SideMenubar from "../components/MyPage_SideMenubar";
+import {createGlobalStyle } from "styled-components";
 
-const MyPage_SideMenubarCss= styled(MyPage_SideMenubar)`
+const MyPage_SideMenubarCss=createGlobalStyle`
+    *{
+        margin:0;
+        padding:0;
+       
+    }
 
+    .sdBar{
+        
+        position:fixed;
+        top:50%;
+        transform:translateY(-50%);
+        left:0;
+        padding:1em;
+        width:200px;
+        height:auto;
+        background-color:yellow;
+    }
 `;
 
 export default MyPage_SideMenubarCss;
