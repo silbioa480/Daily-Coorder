0349ff6ff60f3ea189d1514877be9b9acb510c2a
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-02-24 19:48:04 +0900

    태영: 마이페이지 그래프페이지

---
+++ src/components/MyPage_ChartPage.tsx
@@ -0,0 +1,11 @@
+import Pie_chart from '../components/Pie_chart';
+
+function MyPage_ChartPage(){
+    return (
+        <>
+            <Pie_chart />
+        </>
+    );
+}
+
+export default MyPage_ChartPage;
