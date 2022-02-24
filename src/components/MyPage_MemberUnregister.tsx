186c82c0582163c83796f166ee69e29dc674c02a
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-02-24 13:46:42 +0900

    태영: 회원탈퇴페이지 수정

--- src/components/MyPage_MemberUnregister.tsx
+++ src/components/MyPage_MemberUnregister.tsx
@@ -1,9 +1,13 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function MyPage_MemberUnregister(){
     return (
         <>
-        
+                <div>
+                        <Button>회원 탈퇴</Button>
+
+                </div>
         </>
     );
 }
