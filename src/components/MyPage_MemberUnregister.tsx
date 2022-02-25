49eb399b0216bb614e4140b6fd905dbd88281f82
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-02-25 20:22:29 +0900

    태영: 멤버삭제 페이지 수정

--- src/components/MyPage_MemberUnregister.tsx
+++ src/components/MyPage_MemberUnregister.tsx
@@ -1,13 +1,47 @@
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
+import MyPage_MemberInformation from './MyPage_MemberInformation';
+
 
 function MyPage_MemberUnregister(){
+    const [show,setShow]=useState(false);
+    const handleShow=()=>{
+        setShow(true);
+    }
+    const handleClose=()=>{
+        setShow(false);
+    }
     return (
         <>
-                <div>
-                        <Button>회원 탈퇴</Button>
+                <div style={{backgroundColor:"yellow",width:"60%",margin:"4em auto",height:"750px"}}>
+                    <MyPage_MemberInformation />
+                
+                    <div style={{width:"100%",display:"flex",justifyContent:"center",marginTop:"5em"}}>
+                        <Button variant="primary" onClick={handleShow}>
+                            회원 탈퇴
+                        </Button>
 
+                        <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                            <Modal.Title>회원탈퇴 확인</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>회원탈퇴를 정말 하시겠습니까?</Modal.Body>
+                            <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                이전
+                            </Button>
+                            <Button variant="primary" onClick={handleClose}>
+                            회원 탈퇴
+                            </Button>
+                            </Modal.Footer>
+                        </Modal>
+                    </div>
                 </div>
+
+               
         </>
     );
 }
