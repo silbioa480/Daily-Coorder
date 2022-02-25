11472999491c992ae5c25a4e9c356b7b1e903ecd
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-02-25 20:22:48 +0900

    태영:사이드메뉴바 수정

--- src/components/MyPage_SideMenubar.tsx
+++ src/components/MyPage_SideMenubar.tsx
@@ -1,6 +1,7 @@
 import MyPage_SideMenubarCss from '../css/MyPage_SideMenubarCss';
 import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 
 function isCeo(){
@@ -18,27 +19,29 @@ function MyPage_SideMenubar(){
 
         <>
                 <MyPage_SideMenubarCss />
-                <div className="sdBar">
-                    <Nav defaultActiveKey="/home" className="flex-column">
-                        
-                                <Nav.Link href="/MyPage_MemberInformation">회원 정보</Nav.Link>
-                                <Nav.Link href="/MyPage_MemberModify">회원 정보 수정</Nav.Link>
-                                <Nav.Link href="/MyPage_SizeRegister">체형 등록</Nav.Link>
-                                <Nav.Link href="/MyPage_MemberUnregister">회원 탈퇴</Nav.Link>    
-                    </Nav>
+                <BrowserRouter>
+                    <div className="sdBar">
+                        <Nav defaultActiveKey="/home" className="flex-column">
+                            
+                                    <Nav.Link href="/MyPage_MemberInformation">회원 정보</Nav.Link>
+                                    <Nav.Link href="/MyPage_MemberModify">회원 정보 수정</Nav.Link>
+                                    <Nav.Link href="/MyPage_SizeRegister">체형 등록</Nav.Link>
+                                    <Nav.Link href="/MyPage_MemberUnregister">회원 탈퇴</Nav.Link>    
+                        </Nav>
 
-                    <Nav defaultActiveKey="/home" className="flex-column">     
-                                <Nav.Link href="/MyPage_Adapplication">광고 등록 신청</Nav.Link>
-                                <Nav.Link href="/MyPage_ChartPage">게시물 차트 표시</Nav.Link>
-                    </Nav>
+                        <Nav defaultActiveKey="/home" className="flex-column">     
+                                    <Nav.Link href="/MyPage_Adapplication">광고 등록 신청</Nav.Link>
+                                    <Nav.Link href="/MyPage_ChartPage">게시물 차트 표시</Nav.Link>
+                        </Nav>
 
-                    <Nav defaultActiveKey="/home" className="flex-column">
-                        
-                                <Nav.Link href="/MyPage_AdManagerPage">광고 관리</Nav.Link>
-                                <Nav.Link href="/MyPage_AdProvePage">광고 등록 승인</Nav.Link>
-                        
-                    </Nav>
-                </div>
+                        <Nav defaultActiveKey="/home" className="flex-column">
+                            
+                                    <Nav.Link href="/MyPage_AdManagerPage">광고 관리</Nav.Link>
+                                    <Nav.Link href="/MyPage_AdProvePage">광고 등록 승인</Nav.Link>
+                            
+                        </Nav>
+                    </div>
+                </BrowserRouter>
         </>
     );
 }
