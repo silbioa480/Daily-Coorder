80971296b3c6b940b0afac7087507d7da86008e9
Gayoung Song <98731537+gayoung106@users.noreply.github.com>
2022-02-27 17:38:22 +0900

    가영: 회원가입 링크 및 style수정

--- src/routes/LogIn.js
+++ src/routes/LogIn.js
@@ -1,7 +1,8 @@
 
 import {useState} from "react";
+import { Link } from "react-router-dom";
 
-function Login() {
+function LogIn() {
 
     const [Id, setId] = useState("");
     const [Password, setPassword] = useState("");
@@ -15,6 +16,7 @@ function Login() {
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
+
     };
 
     return (
@@ -22,40 +24,87 @@ function Login() {
             display: 'flex', justifyContent: 'center', alignItems: 'center',
             width: '100%', height: '40vh'
         }}>
-            <form
-                onSubmit={onSubmitHandler}
-                style={{ display: "flex", flexDirection: "column" }}>
-                <h1>회원 로그인</h1>
+            <form onSubmit={onSubmitHandler}>
+                <p style={{display: "flex", flexDirection: "column", fontSize: "25px"}}>로그인</p>
                 <br/><br/>
                 <label>아이디</label>
-                <input type="id" value={Id} placeholder="아이디를 입력해주세요." onChange={onIdHandler} />
-                <br />
+                <input style={{marginTop: "15px", borderRadius: "2px", width:"100%", height:"40px",
+                    border: "1px solid #e5e5e5", padding: "9px 12px", outline: "none", boxSizing: "border-box"
+                }} type="id" value={Id} placeholder="아이디를 입력해주세요." onChange={onIdHandler}/>
+                <br/><br/>
                 <label>비밀번호</label>
-                <input type="password" value={Password} placeholder="비밀번호를 입력해주세요." onChange={onPasswordHanlder} />
-                <br />
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1"/>
-                        <label className="custom-control-label" htmlFor="customCheck1">아이디 저장</label>
+                <input style={{
+                    marginTop: "15px", borderRadius: "2px", width: "100%", height: "40px",
+                    border: "1px solid #e5e5e5", padding: "9px 12px", outline: "none", boxSizing: "border-box"
+                }} type="password" value={Password} placeholder="비밀번호를 입력해주세요." onChange={onPasswordHanlder}/>
+                <br/>
+                {/*<div className="form-group">*/}
+                {/*    <div className="custom-control custom-checkbox">*/}
+                {/*        <input type="checkbox" className="custom-control-input" id="customCheck1"/>*/}
+                {/*        <label style={{*/}
+                {/*            marginTop: "15px", fontSize: "15px"*/}
+                {/*        }} className="custom-control-label" htmlFor="customCheck1">아이디 저장</label>*/}
+                {/*    </div>*/}
+                {/*    <p style={{*/}
+                {/*        marginTop: "5px",  fontSize: "15px"*/}
+                {/*    }} className="forgot-id text-right">*/}
+                {/*        <Link to="#">아이디 / 비밀번호 찾기</Link>*/}
+                {/*    </p>*/}
+                {/*</div>*/}
+                <div style={{
+                    display: "flex", justifyContent: "space-between", alignItems: "center"
+                }} className="loginMid">
+                    <label className="autoLogin" htmlFor="hint">
+                        {" "}
+                        <input type="checkbox" id="hint"/> 로그인 유지하기
+                    </label>
+                    <div style={{
+                        fontSize: "12px", color: "#8d8d8d", lineHeight: 3
+                    }} className="autoLogin"><Link to="#">아이디 / 비밀번호 찾기</Link></div>
+                </div>
 
+                <button style={{
+                    width: "350px", height: "40px", fontSize: "14px", padding: "13px 30px", cursor:"pointer", backgroundColor: "black",
+                    color:"white", lineHeight: "1px", marginTop: "20px", marginBottom: "12px", borderRadius: "3px", borderStyle:"none"
+                }} type="submit" className="btn" value='로그인'>로그인</button>
+                <br/>
+
+                <div style={{
+                    marginBottom: "30px"
+                }} className="socialBox">
+                    <div style={{ backgroundColor: "#feec34", borderColor: "#feec34", height: "40px", display: "flex",
+                        alignItems: "center", justifyContent: "center", boxSizing: "border-box", marginBottom: '10px', borderRadius:"3px"
+                    }} className="kakao">
+                        <img style={{
+                            width: "24px", height: "25px"
+                        }} className="kakaoLogo" src="#"/>
+                        <button style={{
+                            width: "300px", fontSize: "15px", textAlign: "center", display:"inline-block", boxSizing:"border-box"
+                        }} type="submit" className="kakaoText btn" value='로그인'>카카오 계정으로 신규가입</button>
+                    </div>
+                    <div style={{ backgroundColor: "#21538a", borderColor: "#21538a", height: "40px", display: "flex",
+                        alignItems: "center", justifyContent: "center", boxSizing: "border-box", marginBottom: '10px', borderRadius:"3px"
+                    }} className="facebook">
+                        <img style={{
+                            width: "24px", height: "25px"
+                        }} className="facebookLogo" src="#" />
+                        <button style={{
+                            width: "300px", fontSize: "15px", textAlign: "center", display:"inline-block", boxSizing:"border-box"
+                        }} type="submit" className="facebookText btn" value='로그인'>페이스북 계정으로 신규가입</button>
                     </div>
                 </div>
-                <br/>
-
-
-                <button type="submit" className="btn btn-dark btn-sm btn-block" value='로그인'>로그인</button>
-                <br/>
-                <p className="forgot-id text-right">
-                     <a href="#">아이디 찾기</a> <a href="#">비밀번호 찾기</a>
-                </p>
-
-
+                <div style={{
+                    textAlign: "center", fontSize: "10px"
+                }} className="loginEnd">
+                    <div style={{ color: "#bcbcbc", fontSize: "15px", marginBottom:"35px",
+                         cursor: "pointer"
+                    }} className="loginLine">
+                        회원이 아니신가요? <Link to="/signup">회원가입</Link>
+                    </div>
+                </div>
             </form>
         </div>
     )
 }
 
-
-
-
-export default Login;
+export default LogIn;
