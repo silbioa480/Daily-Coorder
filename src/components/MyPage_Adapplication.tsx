c0e454ed0da2ff1dfaf46f944ef807ff6347b5f3
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-02-28 19:57:32 +0900

    태영:관리승인페이지 수정

--- src/components/MyPage_Adapplication.tsx
+++ src/components/MyPage_Adapplication.tsx
@@ -1,7 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image';
+
 
 function MyPage_Adapplication(){
     return (
         <>
+            <div className="AdContainer">
+                    <div className="AdImg">
+                        <Image />
+                    </div>
+                    <div className="Adurl">
+                        
+                    </div>
+                    <div className="Addescription">
+
+                    </div>
+
+            </div>
         </>
     );
 }
