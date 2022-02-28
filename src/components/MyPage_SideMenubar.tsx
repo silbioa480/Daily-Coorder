e0925264bd8ee2b97b78a419db9b0548dcaa0c6d
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-02-28 10:39:48 +0900

    태영: 마이페이지 사이드메뉴바 경로수정

--- src/components/MyPage_SideMenubar.tsx
+++ src/components/MyPage_SideMenubar.tsx
@@ -23,21 +23,21 @@ function MyPage_SideMenubar(){
                 <BrowserRouter>
                     <div className="sdBar">
                         <Nav className="flex-column">
-                                    <Nav.Link href="/MyPage_MemberInformation">회원 정보</Nav.Link>
-                                    <Nav.Link href="/MyPage_MemberModify">회원 정보 수정</Nav.Link>
-                                    <Nav.Link href="/MyPage_SizeRegister">체형 등록</Nav.Link>
-                                    <Nav.Link href="/MyPage_MemberUnregister">회원 탈퇴</Nav.Link>    
+                                    <Nav.Link href="/member/">회원 정보</Nav.Link>
+                                    <Nav.Link href="/member/MyPage_MemberModify">회원 정보 수정</Nav.Link>
+                                    <Nav.Link href="/member/MyPage_SizeRegister">체형 등록</Nav.Link>
+                                    <Nav.Link href="/member/MyPage_MemberUnregister">회원 탈퇴</Nav.Link>    
                         </Nav>
 
                         <Nav className="flex-column">     
-                                    <Nav.Link href="/MyPage_Adapplication">광고 등록 신청</Nav.Link>
-                                    <Nav.Link href="/MyPage_ChartPage">게시물 차트 표시</Nav.Link>
+                                    <Nav.Link href="/member/MyPage_Adapplication">광고 등록 신청</Nav.Link>
+                                    <Nav.Link href="/member/MyPage_ChartPage">게시물 차트 표시</Nav.Link>
                         </Nav>
 
                         <Nav className="flex-column">
                             
-                                    <Nav.Link href="/MyPage_AdManagerPage">광고 관리</Nav.Link>
-                                    <Nav.Link href="/MyPage_AdProvePage">광고 등록 승인</Nav.Link>
+                                    <Nav.Link href="/member/MyPage_AdManagerPage">광고 관리</Nav.Link>
+                                    <Nav.Link href="/member/MyPage_AdProvePage">광고 등록 승인</Nav.Link>
                             
                         </Nav>
                     </div>
