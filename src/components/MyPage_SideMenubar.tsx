7e9cd33977ef8dfe6f38e2e342b34b16be21637a
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-02-28 09:49:56 +0900

    태영: 마이페이지 사이드메뉴바수정

--- src/components/MyPage_SideMenubar.tsx
+++ src/components/MyPage_SideMenubar.tsx
@@ -2,6 +2,7 @@ import MyPage_SideMenubarCss from '../css/MyPage_SideMenubarCss';
 import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import { NavButton } from '../css/NavStyle';
 
 
 function isCeo(){
@@ -21,20 +22,19 @@ function MyPage_SideMenubar(){
                 <MyPage_SideMenubarCss />
                 <BrowserRouter>
                     <div className="sdBar">
-                        <Nav defaultActiveKey="/home" className="flex-column">
-                            
+                        <Nav className="flex-column">
                                     <Nav.Link href="/MyPage_MemberInformation">회원 정보</Nav.Link>
                                     <Nav.Link href="/MyPage_MemberModify">회원 정보 수정</Nav.Link>
                                     <Nav.Link href="/MyPage_SizeRegister">체형 등록</Nav.Link>
                                     <Nav.Link href="/MyPage_MemberUnregister">회원 탈퇴</Nav.Link>    
                         </Nav>
 
-                        <Nav defaultActiveKey="/home" className="flex-column">     
+                        <Nav className="flex-column">     
                                     <Nav.Link href="/MyPage_Adapplication">광고 등록 신청</Nav.Link>
                                     <Nav.Link href="/MyPage_ChartPage">게시물 차트 표시</Nav.Link>
                         </Nav>
 
-                        <Nav defaultActiveKey="/home" className="flex-column">
+                        <Nav className="flex-column">
                             
                                     <Nav.Link href="/MyPage_AdManagerPage">광고 관리</Nav.Link>
                                     <Nav.Link href="/MyPage_AdProvePage">광고 등록 승인</Nav.Link>
