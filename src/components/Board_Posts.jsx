c2304c08d1ea4107a2777636978dabf964c84946
INYONGSIK <kiuyyyy@naver.com>
2022-02-28 15:26:37 +0900

    게시판 구조,댓글textarea수정

--- src/components/Board_Posts.jsx
+++ src/components/Board_Posts.jsx
@@ -1,19 +1,25 @@
 import "../css/Board_Posts.css";
-import { BsDownload, BsBookmarks, BsPlusSquare } from "react-icons/bs";
+import {
+  BsDownload,
+  BsBookmarks,
+  BsPlusSquare,
+  BsArrowDownShort,
+} from "react-icons/bs";
 import { FiMoreHorizontal, FiX } from "react-icons/fi";
 import { HiOutlineThumbUp } from "react-icons/hi";
 import { RiShareForwardLine } from "react-icons/ri";
-import jini from "../img/jini.png";
 import qr from "../img/qr.png";
 import { Circle, Com, Square } from "../css/Board_Posts";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import similar from "../img/similar.png";
+import { useRef } from "react";
+import React from "react";
 
 function Board_Picture() {
   return (
     <div className="bp">
       <div className="picture">
-        <img src={jini} alt="profile" />
+        <img src={similar} alt="" />
       </div>
       <div className="bp1">
         <Square style={{ marginTop: "10px" }}>20대</Square>
@@ -35,47 +41,154 @@ function Board_Picture() {
   );
 }
 
-function Information() {
+function Download() {
   return (
-    <div className="infor">
-      <Drop />
-      <Qrcode />
-      <Comment />
+    <div style={{ padding: "10px" }}>
+      <Drop />;
+    </div>
+  );
+}
+
+function Follow() {
+  return (
+    <div className="infor" style={{ display: "flex" }}>
+      <div style={{ width: "300px", display: "flex" }}>
+        <div>
+          <a href="#">
+            <Circle src={similar} style={{ marginLeft: "20px" }}></Circle>
+          </a>
+        </div>
+        <div style={{ margin: "auto 0", marginLeft: "7px" }}>
+          <a href="#">
+            <p>닉네임</p>
+          </a>
+          <p>팔로워 x명</p>
+        </div>
+      </div>
+
+      <div>
+        <button
+          style={{
+            width: "80px",
+            height: "48px",
+            border: "none",
+            borderRadius: "24px",
+          }}
+          // 팔로우누으면 팔로잉으로 바뀌고 backgroundc색 조정
+        >
+          팔로우
+        </button>
+      </div>
     </div>
   );
 }
 
 function Comment() {
+  const textRef = React.createRef();
+  const textResize = () => {
+    textRef.current.style.height = "auto";
+    textRef.current.style.height = textRef.current.scrollHeight + "px";
+  };
+
+  // const ref = React.createRef();
+  // const textResize = useCallback(() => {
+  //   if (ref === null || ref.current === null) {
+  //     return;
+  //   }
+  //   ref.current.style.height = "auto";
+  //   ref.current.style.height = ref.current.scrollHeight + "px";
+  // }, []);
   return (
-    <table>
-      <tr>
+    <div className="t-wrapper">
+      <div className="c-wrapper">
+        <h1
+          style={{
+            fontsize: "20px",
+            fontWeight: "700",
+            width: "40px",
+            height: "25px",
+            textAlign: "center",
+            padding: "0.2em",
+          }}
+        >
+          댓글
+        </h1>
+        <BsArrowDownShort
+          style={{ width: "40px", height: "25px" }}
+        ></BsArrowDownShort>
+      </div>
+      {/* 댓글작성시 댓글생김 */}
+      <form>
         <div className="wrapper">
-          <td>
-            <Circle src={similar}></Circle>
-          </td>
-          <td>
-            <textarea
-              placeholder="댓글을 입력하세요"
-              cols={35}
-              required
-            ></textarea>
-          </td>
+          <label for="comment">
+            <Circle
+              src={similar}
+              style={{ display: "inline", marginLeft: "20px" }}
+            ></Circle>
+          </label>
+          <textarea
+            ref={textRef}
+            onKeyUp={textResize}
+            onKeyDown={textResize}
+            id="comment"
+            placeholder="댓글을 입력하세요"
+            cols={35}
+            required
+            style={{ marginLeft: "10px" }}
+          ></textarea>
+          <div
+            className="comment_btn"
+            style={{ display: "flex", padding: "5px" }}
+          >
+            <button style={{ marginLeft: "auto" }}>취소</button>
+            <button
+              type="submit"
+              style={{ marginRight: "20px", marginLeft: "7px" }}
+            >
+              완료
+            </button>
+          </div>
         </div>
-      </tr>
-      <tr>
-        <div className="comment_btn">
-          <td>
-            <button>취소</button>
-          </td>
-          <br></br>
-        </div>
-        <div className="comment_btn2">
-          <td>
-            <button type="submit">완료</button>
-          </td>
-        </div>
-      </tr>
-    </table>
+      </form>
+    </div>
+
+    // <table>
+    //   <h1>
+    //     댓글
+    //     <BsArrowDownShort />
+    //   </h1>
+    //   <div className="t-wrapper">
+    //     <tr>
+    //       <div className="wrapper">
+    //         <td>
+    //           <Circle src={similar}></Circle>
+    //         </td>
+    //         <td>
+    //           <textarea
+    //             // onKeyDown={textResize}
+    //             // onKeyDown={textResize}
+    //             placeholder="댓글을 입력하세요"
+    //             cols={35}
+    //             required
+    //           ></textarea>
+    //         </td>
+    //       </div>
+    //     </tr>
+    //     <tr>
+    //       <div className="comment_btn">
+    //         <td>
+    //           <button style={{ display: "inline" }}>취소</button>
+    //         </td>
+    //         <br></br>
+    //       </div>
+    //       <div className="comment_btn2">
+    //         <td>
+    //           <button type="submit">완료</button>
+    //         </td>
+    //       </div>
+    //     </tr>
+    //   </div>
+    // </table>
   );
 }
 
@@ -121,7 +234,11 @@ function Board_Posts() {
   return (
     <div className="Board_Posts">
       <Board_Picture />
-      <Information />
+      <div>
+        <Download />
+        <Follow />
+        <Comment />
+      </div>
     </div>
   );
 }
--- src/css/Board_Posts.tsx
+++ src/css/Board_Posts.tsx
@@ -2,37 +2,36 @@ import styled from "styled-components";
 
 //프로필
 export const Circle = styled.img`
-border-radius: 50%;
-font-size:1em;
-border: 1px solid lightgray;
-color: gray;
-background: white;
-width: 40px;
-height: 40px;
-display:flex;
-justify-content:center;
-align-items:center;
-`
+  border-radius: 50%;
+  font-size: 1em;
+  border: 1px solid lightgray;
+  color: gray;
+  background: white;
+  width: 48px;
+  height: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 //댓글
 export const Com = styled.div`
-font-size:1em;
-border: 1px solid lightgray;
-color: black;
-background: white;
-width: 200px;
-height: 50px;
-margin-left:60px;
+  font-size: 1em;
+  border: 1px solid lightgray;
+  color: black;
+  background: white;
+  width: 200px;
+  height: 50px;
+  margin-left: 60px;
 `;
 
-export const Square=styled.div`
-font-size:1em;
-border: 1px solid lightgray;
-color: black;
-width:40px;
-height:30px;
-background: white;
-display:flex;
-justify-content:center;
-align-items:center;
+export const Square = styled.div`
+  font-size: 1em;
+  border: 1px solid lightgray;
+  color: black;
+  width: 40px;
+  height: 30px;
+  background: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
-
