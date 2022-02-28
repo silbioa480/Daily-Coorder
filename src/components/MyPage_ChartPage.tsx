79fd194e9568b948cbd3586dbbcaeeadeb111293
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-02-28 19:58:19 +0900

    태영:차트페이지 수정

--- src/components/MyPage_ChartPage.tsx
+++ src/components/MyPage_ChartPage.tsx
@@ -1,21 +1,93 @@
 import Pie_chart from '../components/Pie_chart';
 import { Button } from 'react-bootstrap';
 import { useState } from 'react';
-
+import { Card } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import CloseButton from 'react-bootstrap/CloseButton';
 
 
 function MyPage_ChartPage(){
     const [show,setShow]=useState(false);
     const [hide,setHide]=useState(true);
-
+    const [close,setClose]=useState(false);
     const HandleClick=()=>{
         setShow(true);
         setHide(false);
+        setClose(true);
+    }
+
+    const handleClose=()=>{
+        setClose(false);
+        if(close==false){
+            
+        }
     }
     return (
         <>
-               {hide && <Button onClick={HandleClick}>상세 차트 표시</Button>}
-               {show && <Pie_chart />}
+               
+               
+               <Container style={{display:"flex"}}>
+                {<Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </Card.Text>
+                            {hide && <Button onClick={HandleClick}>상세 차트 표시</Button>}
+                            {show && <Pie_chart  />}
+                            {close && <CloseButton onClick={handleClose}/>}
+                        </Card.Body>
+                    </Card>}
+
+
+                    {<Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </Card.Text>
+                            {hide && <Button onClick={HandleClick}>상세 차트 표시</Button>}
+                            {show && <Pie_chart />}
+                            {close && <CloseButton onClick={handleClose}/>}
+                        </Card.Body>
+                    </Card>}
+
+                    {<Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </Card.Text>
+                            {hide && <Button onClick={HandleClick}>상세 차트 표시</Button>}
+                            {show && <Pie_chart />}
+                            {close && <CloseButton onClick={handleClose}/>}
+                        </Card.Body>
+                    </Card>}
+                 
+
+                    {<Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </Card.Text>
+                            {hide && <Button onClick={HandleClick}>상세 차트 표시</Button>}
+                            {show && <Pie_chart />}
+                            {close && <CloseButton onClick={handleClose}/>}
+                        </Card.Body>
+                    </Card>}
+                    
+               </Container>
+
+                
         </>
     );
 }
