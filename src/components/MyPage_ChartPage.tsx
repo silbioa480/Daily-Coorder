65c31fb85f34937da8ec9c9ba4ca864ee9eb01f2
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-03-02 20:13:33 +0900

    태영:차트페이지 수정

--- src/components/MyPage_ChartPage.tsx
+++ src/components/MyPage_ChartPage.tsx
@@ -1,90 +1,61 @@
-import Pie_chart from '../components/Pie_chart';
+
 import { Button } from 'react-bootstrap';
 import { useState } from 'react';
 import { Card } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
-import CloseButton from 'react-bootstrap/CloseButton';
+import { Link } from 'react-router-dom';
+
+
+
+
+function CardStyle(){
+    const [hashtag,setHashtag]=useState("");
+    
+    const [hide,setHide]=useState(true);
+   
+    const HandleClick=()=>{
+        setHide(false);    
+    }
+
+    return (
+            <>
+                {hide && <Card style={{ width: '18rem',height:"400px" }}>
+                        <Card.Img variant="top" src="holder.js/100px180" alt="clothes image" style={{marginTop:"3vh"}} />
+                        <Card.Body >
+                            <Card.Title style={{textAlign:"center",margin:"3vh 0"}}>의류 타이틀</Card.Title>
+                            <Card.Text style={{textAlign:"center",marginBottom:"3vh"}}>
+                                    의류 관련내용
+                            </Card.Text>
+                            <Card.Text style={{textAlign:"center",marginBottom:"3vh"}}>
+                                {hashtag}해시태그
+                            </Card.Text>
+                            <div style={{width:"100%",display:"flex",justifyContent:"center"}}>
+                                <Button onClick={HandleClick} style={{textAlign:"center"}}>
+                                        <Link to="/member/MyPage_ChartPage/MyPage_detailChartPage" style={{color:"white"}}>
+                                            상세 차트 표시
+                                        </Link>
+                                </Button>
+                            </div>
+                        </Card.Body>
+                    </Card>}
+            </>
+
+    );
+}
 
 
 function MyPage_ChartPage(){
-    const [show,setShow]=useState(false);
-    const [hide,setHide]=useState(true);
-    const [close,setClose]=useState(false);
-    const HandleClick=()=>{
-        setShow(true);
-        setHide(false);
-        setClose(true);
-    }
-
-    const handleClose=()=>{
-        setClose(false);
-        if(close==false){
-            
-        }
-    }
+    const [clothesCard,setClothesCard]=useState(["sadf","sadf","sdef","asdf"]);
     return (
         <>
                
                
-               <Container style={{display:"flex"}}>
-                {<Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            {hide && <Button onClick={HandleClick}>상세 차트 표시</Button>}
-                            {show && <Pie_chart  />}
-                            {close && <CloseButton onClick={handleClose}/>}
-                        </Card.Body>
-                    </Card>}
-
-
-                    {<Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            {hide && <Button onClick={HandleClick}>상세 차트 표시</Button>}
-                            {show && <Pie_chart />}
-                            {close && <CloseButton onClick={handleClose}/>}
-                        </Card.Body>
-                    </Card>}
-
-                    {<Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            {hide && <Button onClick={HandleClick}>상세 차트 표시</Button>}
-                            {show && <Pie_chart />}
-                            {close && <CloseButton onClick={handleClose}/>}
-                        </Card.Body>
-                    </Card>}
-                 
-
-                    {<Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            {hide && <Button onClick={HandleClick}>상세 차트 표시</Button>}
-                            {show && <Pie_chart />}
-                            {close && <CloseButton onClick={handleClose}/>}
-                        </Card.Body>
-                    </Card>}
-                    
+               <Container style={{display:"flex",justifyContent:"space-around",height:"auto"}} className="bg-primary">
+                        {clothesCard.map(()=>{
+                            return (
+                                <CardStyle />
+                            );
+                        })}
                </Container>
 
                 
