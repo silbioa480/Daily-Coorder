0ff19c3340e8f41791481ea5cb45a69334c56813
silbioa480 <75172264+silbioa480@users.noreply.github.com>
2022-03-02 20:52:39 +0900

    Update NotLogInNav.tsx

--- src/components/NotLogInNav.tsx
+++ src/components/NotLogInNav.tsx
@@ -3,8 +3,8 @@ import { NavButton } from "../css/NavStyle";
 function NotLogInNav() {
   return (
     <>
-      <NavButton to="login">로그인</NavButton>
-      <NavButton to="signup">회원가입</NavButton>
+      <NavButton to="/login">로그인</NavButton>
+      <NavButton to="/signup">회원가입</NavButton>
     </>
   );
 }
