0130bef47baf64cb502e671f35a0bf8d617d8fd6
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-03-02 20:14:10 +0900

    태영:차트상세페이지 수정

---
+++ src/components/MyPage_detailChartPage.tsx
@@ -0,0 +1,36 @@
+import Pie_chart from "./Pie_chart";
+import ClothesDetailCss from "../css/MyPage_detailPageCss";
+import { Image } from "react-bootstrap";
+
+function MyPage_detailChartPage(){
+    return(
+        <>
+            <ClothesDetailCss />
+            <div className="detailContainer">
+
+                    <div className="dataContainer">
+                            <div className="dataimage">
+                                <Image src=""/>
+                            </div>
+                            <div className="datadescri">
+                                <div>
+                                    의류 관련 타이틀
+                                </div>
+                                <div>
+                                    의류 관련 업로드한사람
+                                </div>
+                                <div>의류 관련 태그</div>
+                                <div>
+                                    의류 관련 내용
+                                </div>
+                            </div>
+                    </div>
+                    <div className="chartContainer">
+                        <Pie_chart />
+                    </div> 
+            </div>
+        </>
+    );
+}
+
+export default MyPage_detailChartPage;
