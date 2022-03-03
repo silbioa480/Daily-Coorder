5511214dad553dd8476d3730ffc4f34578f90a66
Gayoung Song <98731537+gayoung106@users.noreply.github.com>
2022-03-03 12:12:28 +0900

    가영: 아이디 찾기 수정

--- src/components/IdSearch.js
+++ src/components/IdSearch.js
@@ -4,7 +4,9 @@ import $ from "jquery";
 
 function IdSearch() {
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [name, setName] = useState("");
   const [errorPhoneNumber, setErrorPhoneNumber] = useState(false);
+  const [errorName, setErrorName] = useState(false);
 
   const onChangePhoneNumber = (e) => {
     const phoneNumberRegex = /^[0-9\b -]{0,13}$/;
@@ -14,17 +16,38 @@ function IdSearch() {
     setPhoneNumber(e.target.value);
   };
 
+  const onNameHandler = (e) => {
+    const nameRegex = /[0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/g;
+    if (!e.target.value || nameRegex.test(e.target.value)) setErrorName(false);
+    else setErrorName(true);
+    setName(e.currentTarget.value);
+  };
+
+  const checkName = $("#name").val();
   const checkPhoneNumber = $("#phoneNumber").val();
+
   const onSubmitHandler = (e) => {
     e.preventDefault();
 
+    if (!checkName) {
+      alert("이름을 입력해주세요");
+      $("#name").val("");
+      $("#name").focus();
+      return false;
+    }
+    $("#name").removeClass("borderErr");
+
     if (!checkPhoneNumber) {
-      alert("아이디를 입력해주세요");
+      alert("휴대폰 번호를 입력해주세요");
       $("#phoneNumber").val("");
       $("#phoneNumber").focus();
       return false;
     }
     $("#phoneNumber").removeClass("borderErr");
+  };
+
+  const nameKeyPress = (e) => {
+    $("#name").removeClass("borderErr");
   };
   const phoneNumberKeyPress = (e) => {
     $("#phoneNumber").removeClass("borderErr");
@@ -59,6 +82,42 @@ function IdSearch() {
           <br />
           <br />
 
+          <div>
+            <label htmlFor="user-name">이름</label>
+            <input
+              style={{
+                marginTop: "10px",
+                borderRadius: "2px",
+                width: "100%",
+                height: "40px",
+                border: "1px solid #e5e5e5",
+                padding: "9px 12px",
+                outline: "none",
+                boxSizing: "border-box",
+                marginBottom: "10px",
+              }}
+              id="name"
+              name="user-name"
+              type="text"
+              placeholder="이름"
+              value={name}
+              onKeyPress={nameKeyPress}
+              onChange={onNameHandler}
+              className="loginregister_input"
+            />{" "}
+            {errorName && (
+              <div
+                className="invalid-input"
+                style={{
+                  color: "deepskyblue",
+                  fontSize: "12px",
+                  marginBottom: "10px",
+                }}
+              >
+                이름 형식이 올바르지 않습니다.
+              </div>
+            )}
+          </div>
           <div>
             <label
               htmlFor="user-phone"
