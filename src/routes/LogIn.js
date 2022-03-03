ba6d7a5d51b0606e051d9bef844743089c52b3ff
Gayoung Song <98731537+gayoung106@users.noreply.github.com>
2022-03-04 00:16:47 +0900

    가영: 경로고침 <BrowserRouter>를 빼야된대요!

--- src/routes/LogIn.js
+++ src/routes/LogIn.js
@@ -1,28 +1,26 @@
 import IdSearch from "../components/IdSearch";
 import PasswordSearch from "../components/PasswordSearch";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import LoginMember from "../components/LoginMember";
 import SignUp from "./SignUp";
 
 function LogIn() {
   return (
     <>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/login/IdSearch">
-            <IdSearch />
-          </Route>
-          <Route exact path="/login/PasswordSearch">
-            <PasswordSearch />
-          </Route>
-          <Route exact path="/signup">
-            <SignUp />
-          </Route>
-          <Route exact path="/login">
-            <LoginMember />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/login/IdSearch">
+          <IdSearch />
+        </Route>
+        <Route exact path="/login/PasswordSearch">
+          <PasswordSearch />
+        </Route>
+        <Route exact path="/signup">
+          <SignUp />
+        </Route>
+        <Route exact path="/login">
+          <LoginMember />
+        </Route>
+      </Switch>
     </>
   );
 }
