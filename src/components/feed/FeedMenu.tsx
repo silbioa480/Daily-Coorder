8b26af7a4237e4fa9dcb7dc342ba9bef02663d4f
silbioa480 <silbioa480@gmail.com>
2022-03-04 14:35:16 +0900

    changed FeedMenu to Nav

--- src/components/feed/FeedMenu.tsx
+++ src/components/feed/FeedMenu.tsx
@@ -1,51 +1,27 @@
-import { ListGroup } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function FeedMenu() {
   return (
-    <ListGroup as="ul">
-      <ListGroup.Item as="li">
-        <Link
-          to={"/feed/"}
-          style={{
-            display: "inline-block",
-            width: "100%",
-            padding: "10px 0",
-            textAlign: "center",
-          }}
-        >
+    <Nav style={{ position: "sticky" }}>
+      <Nav.Item>
+        <Nav.Link as={Link} to="/feed">
           팔로워
-        </Link>
-      </ListGroup.Item>
+        </Nav.Link>
+      </Nav.Item>
 
-      <ListGroup.Item as="li">
-        <Link
-          to={"/feed/my_liked"}
-          style={{
-            display: "inline-block",
-            width: "100%",
-            padding: "10px 0",
-            textAlign: "center",
-          }}
-        >
+      <Nav.Item>
+        <Nav.Link as={Link} to="/feed">
           내가 좋아요한 게시물
-        </Link>
-      </ListGroup.Item>
+        </Nav.Link>
+      </Nav.Item>
 
-      <ListGroup.Item as="li">
-        <Link
-          to="/feed/my_uploaded"
-          style={{
-            display: "inline-block",
-            width: "100%",
-            padding: "10px 0",
-            textAlign: "center",
-          }}
-        >
+      <Nav.Item>
+        <Nav.Link as={Link} to="/feed">
           내가 업로드한 게시물
-        </Link>
-      </ListGroup.Item>
-    </ListGroup>
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
   );
 }
 
