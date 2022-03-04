0789c230524c4872afd4d43d69365b2e43cd96a8
silbioa480 <silbioa480@gmail.com>
2022-03-04 13:39:18 +0900

    종호: make feed page, divide menu bar and route main

--- src/routes/Feed.tsx
+++ src/routes/Feed.tsx
@@ -1,5 +1,33 @@
+import { Route, Switch } from "react-router-dom";
+import MyLiked from "../components/feed/MyLiked";
+import MyUploaded from "../components/feed/MyUploaded";
+import MyFollowers from "../components/feed/MyFollowers";
+import FeedMenu from "../components/feed/FeedMenu";
+
 function Feed() {
-  return <h1>This is JH.</h1>;
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ width: "30%" }}>
+        <FeedMenu />
+      </div>
+
+      <div style={{ width: "70%", display: "flex", justifyContent: "center" }}>
+        <Switch>
+          <Route path="/feed/my_liked">
+            <MyLiked />
+          </Route>
+
+          <Route path="/feed/my_uploaded">
+            <MyUploaded />
+          </Route>
+
+          <Route path="/feed/">
+            <MyFollowers />
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  );
 }
 
 export default Feed;
