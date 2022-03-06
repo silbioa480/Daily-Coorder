1c4cfd4577770416156590c3c60757aefa00a8b0
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-03-06 17:55:37 +0900

    태영: 정보수정페이지 수정

--- src/components/MyPage_MemberModify.tsx
+++ src/components/MyPage_MemberModify.tsx
@@ -7,35 +7,84 @@ import Modal from 'react-bootstrap/Modal';
 import { useState } from 'react';
 import { Figure } from 'react-bootstrap';
 
+
+
 function MyPage_MemberModify(){
     const [show,setShow]=useState(false);
+    const [userId,setId]=useState("user1234");
+    const [userNickname,setNickname]=useState("");
+    const [userPassword,setPassword]=useState("");
+    const [userPhone,setPhone]=useState("");
+    const [userEmail,setEmail]=useState("");
+
+    const [userInfo,setUserInfo]=useState({
+        id:"",
+        password:"",
+        nickname:"",
+        email:"",
+        phone:""
+    });
+    
     const handleShow=()=>{
         setShow(true);
     }
     const handleClose=()=>{
         setShow(false);
+    } 
+
+    const handleSubmit=(event:any)=>{
+        event.preventDefalut();
     }
 
+   const handleChange=(event:any)=>{
+        const name=event.target.name;
+        const value=event.target.value;
 
+        setUserInfo((userinfo)=>({...userinfo,[name]:[value]}));
+
+   }
+
+   const passwordCheck=(event:any)=>{
+       const comparePwd=event.target.value;
+       if(userInfo.password!=comparePwd){
+           console.log("비밀번호가 다릅니다");
+       }
+   }
+
+   const compareIdCheck=(event:any) =>{
+       if(userInfo.id==userId){
+           alert("이미존재하는 아이디입니다.");
+       }else{
+           alert("사용가능한 아이디입니다.");
+       }
+   }
+    
+   const compareNickName=(event : any)=>{
+        if(userInfo.nickname==userNickname){
+            alert("이미존재하는 닉네임입니다.");
+        }else{
+            alert("사용가능한 닉네임입니다.");
+        }
+   }
     return(
         <>
             <ModifyCss />
             <div className="containerCss">
-                <Form className="pl-5">
+                <Form className="pl-5" onSubmit={handleSubmit}>
                     <Row className="my-5">
                         <Col>
-                                <div style={{width:"170px",height:"150px",margin:"3vh 0 0 0",border:"1px solid black"}}>
-                                    <Figure style={{width:"170px",height:"150px"}}>
+                                <div style={{width:"170px",height:"200px",margin:"3vh 0 0 0",border:"1px solid #dbdbdb"}}>
+                                    <Figure style={{width:"170px",height:"200px"}}>
                                         <Figure.Image
-                                            width={140}
-                                            height={160}
+                                            width={170}
+                                            height={200}
                                             alt="프로필 사진"
-                                            src="holder.js/140x160"
+                                            src="holder.js/170x200"
                                         />
                                     </Figure>
                                 </div>
                                 <div style={{margin:"1vh 0"}}>
-                                    <label className="btn btn-primary" style={{overflow:"hidden"}}>
+                                    <label className="btn btn-white" style={{overflow:"hidden",border:"1px solid #dbdbdb",fontWeight:"bold",padding:".3vw .75vw",borderRadius:"5px"}}>
                                         프로필 사진 업로드<input type="file" style={{display:"none"}}/>
                                     </label>
                                 </div>
@@ -43,8 +92,8 @@ function MyPage_MemberModify(){
                                 <Form.Group  controlId="formGridEmail">
                                         <Form.Label>ID</Form.Label>
                                         <div style={{width:"100%",display:"flex"}}>
-                                            <Form.Control type="text" placeholder="Enter ID"/>
-                                            <Button style={{width:"180px",textAlign:"center"}}>중복 확인</Button>
+                                            <Form.Control type="text" placeholder="Enter ID" name="id" value={userInfo.id} onChange={handleChange}/>
+                                            <Button style={{width:"180px",textAlign:"center"}}  onClick={compareIdCheck}>중복 확인</Button>
                                         </div>
                                 
                                 </Form.Group>
@@ -53,31 +102,31 @@ function MyPage_MemberModify(){
                             <Form.Group  controlId="formGridEmail" style={{marginTop:"1vh"}}>
                                     <Form.Label>닉네임</Form.Label>
                                     <div style={{width:"100%",display:"flex"}}>
-                                        <Form.Control type="text" placeholder="Enter NickName"/>
+                                        <Form.Control type="text" placeholder="Enter NickName" name="nickname" value={userInfo.nickname} onChange={handleChange}/>
                                         <Button style={{width:"180px",textAlign:"center"}}>닉네임중복 확인</Button>
                                     </div>
                             </Form.Group>
                         
                             <Form.Group className="my-3" controlId="formGridPassword">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Form.Control type="password" placeholder="Password" name="password" value={userInfo.password} onChange={handleChange}/>
                             </Form.Group>
 
                             <Form.Group className="my-3" controlId="formGridPassword">
                                 <Form.Label>Password check</Form.Label>
-                                <Form.Control type="password" placeholder="Password check" />
+                                <Form.Control type="password" placeholder="Password check" name="passwordCheck" onChange={passwordCheck}/>
                             </Form.Group>
                     
                             <Form.Group className="my-3">
                                 <Form.Label>Birth</Form.Label>
-                                <Form.Control type="text" placeholder="생년월일 6자리입력" />
+                                <Form.Control type="text" placeholder="생년월일 6자리입력" name="birth"/>
                             </Form.Group>
 
 
                             <Form.Group className="my-3">
                                 <Form.Label>Phone Number</Form.Label>
                                 <div style={{width:"100%",display:"flex"}}>
-                                    <Form.Control type="text" placeholder="Phone Number" />
+                                    <Form.Control type="text" placeholder="Phone Number" name="phoneNumber" value={userInfo.phone} onChange={handleChange}/>
                                     <Button style={{width:"180px",textAlign:"center"}}>인증요청</Button>
                                 </div>
                             </Form.Group>
@@ -85,7 +134,7 @@ function MyPage_MemberModify(){
                             <Form.Group className="my-3">
                                 <Form.Label>Email</Form.Label>
                                 <div style={{width:"100%",display:"flex"}}>
-                                    <Form.Control type="email" placeholder="Enter Email" />
+                                    <Form.Control type="email" placeholder="Enter Email" name="email" value={userInfo.email} onChange={handleChange}/>
                                     <Button style={{width:"180px",textAlign:"center"}}>인증요청</Button>
                                 </div>
                             </Form.Group>
