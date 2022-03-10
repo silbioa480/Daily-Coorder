2651a63635090e4329c3032b59c081d136df6893
Blation <kisn3089@naver.com>
2022-03-10 18:35:31 +0900

    animation all

--- src/components/IdSearch.js
+++ src/components/IdSearch.js
@@ -1,163 +1,165 @@
-import { Link } from "react-router-dom";
-import React, { useState } from "react";
+import {Link} from "react-router-dom";
+import React, {useState} from "react";
 import $ from "jquery";
 import PopUp from "./PopUp";
 import "../css/Search.css";
+import "../css/main/animation.css";
 
 function IdSearch() {
-  const [popup, setPopup] = useState({
-    open: false,
-    title: "",
-    message: "",
-    callback: false,
-  });
-
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [name, setName] = useState("");
-  const [errorPhoneNumber, setErrorPhoneNumber] = useState(false);
-  const [errorName, setErrorName] = useState(false);
-
-  const onChangePhoneNumber = (e) => {
-    const phoneNumberRegex = /^[0-9\b -]{0,13}$/;
-    if (!e.target.value || phoneNumberRegex.test(e.target.value))
-      setErrorPhoneNumber(false);
-    else setErrorPhoneNumber(true);
-    setPhoneNumber(e.target.value);
-  };
-
-  const onNameHandler = (e) => {
-    setName(e.currentTarget.value);
-  };
-  const validation4 = () => {
-    if (!name) setErrorName(true);
-    if (!phoneNumber) setErrorPhoneNumber(true);
-
-    if (name && phoneNumber) return true;
-    else return false;
-  };
-
-  const checkName = $("#name").val();
-  const validCheckName = () => {
-    if (!checkName) {
-      $("#name").val("");
-      $("#name").focus();
-      return true;
-    }
-  };
-  const checkPhoneNumber = $("#phoneNumber").val();
-  const validCheckPhoneNumber = () => {
-    if (!checkPhoneNumber) {
-      $("#phoneNumber").val("");
-      $("#phoneNumber").focus();
-      return true;
-    }
-  };
-
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-
-    if (validCheckName("")) {
-      setPopup({
-        open: true,
-        title: "아이디 찾기 실패ㅠㅠㅠㅠ",
-        message: "이름을 입력해주세요!!",
-      });
-      return;
-    }
-
-    if (validCheckPhoneNumber("")) {
-      setPopup({
-        open: true,
-        title: "아이디 찾기 실패ㅠㅠㅠㅠ",
-        message: "휴대폰 번호를 입력해주세요!!",
-      });
-      return;
-    }
-
-    if (!validation4()) {
-      setPopup({
-        open: true,
-        title: "아이디 찾기 실패ㅠㅠㅠㅠ",
-        message: "정보가 일치하지 않아요!!",
-      });
-      return;
-    } else {
-    }
-    setPopup({
-      open: true,
-      title: "아이디 찾기 성공!!",
-      message: "회원님의 아이디는 '0000' 입니다!!",
-      callback: function () {},
+    const [popup, setPopup] = useState({
+        open: false,
+        title: "",
+        message: "",
+        callback: false,
     });
-    if (validation4()) return;
-  };
 
-  return (
-    <>
-      <div className="search_signup">
-        <PopUp
-          open={popup.open}
-          setPopup={setPopup}
-          message={popup.message}
-          title={popup.title}
-          callback={popup.callback}
-        />
-        <form onSubmit={onSubmitHandler}>
-          <p className="search_upper_font"> 아이디 찾기 </p>
-          <br />
-          <br />
-          <div className="blue-line-out">
-            <div className="blue-line-in"></div>
-          </div>
-          <div className="yellow-line-out">
-            <div className="yellow-line-in"></div>
-          </div>
-          <div>
-            <label htmlFor="user-name" className="search_font">
-              이름
-            </label>
-            <input
-              className="search_inputs"
-              id="name"
-              name="user-name"
-              type="text"
-              placeholder="이름"
-              value={name}
-              onChange={onNameHandler}
-            />{" "}
-          </div>
-          <div>
-            <label className="search_font" htmlFor="user-phone">
-              휴대폰 번호
-            </label>
-            <input
-              className="search_inputs"
-              id="phoneNumber"
-              name="user-phone"
-              type="text"
-              placeholder="-를 제외하고 숫자만 입력하세요"
-              value={phoneNumber}
-              onChange={onChangePhoneNumber}
-            />
-            {errorPhoneNumber && (
-              <div className="search_input_valid">숫자만 입력하세요</div>
-            )}
-          </div>
-          <div>
-            <input
-              type="submit"
-              className="search_button"
-              value="아이디 찾기"
-            />
-          </div>
-          <div className="search_search">
-            <Link to="/login/PasswordSearch">비밀번호 찾기</Link> ||
-            <Link to="/signup"> 회원가입 </Link>
-          </div>
-        </form>
-      </div>
-    </>
-  );
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [name, setName] = useState("");
+    const [errorPhoneNumber, setErrorPhoneNumber] = useState(false);
+    const [errorName, setErrorName] = useState(false);
+
+    const onChangePhoneNumber = (e) => {
+        const phoneNumberRegex = /^[0-9\b -]{0,13}$/;
+        if (!e.target.value || phoneNumberRegex.test(e.target.value))
+            setErrorPhoneNumber(false);
+        else setErrorPhoneNumber(true);
+        setPhoneNumber(e.target.value);
+    };
+
+    const onNameHandler = (e) => {
+        setName(e.currentTarget.value);
+    };
+    const validation4 = () => {
+        if (!name) setErrorName(true);
+        if (!phoneNumber) setErrorPhoneNumber(true);
+
+        if (name && phoneNumber) return true;
+        else return false;
+    };
+
+    const checkName = $("#name").val();
+    const validCheckName = () => {
+        if (!checkName) {
+            $("#name").val("");
+            $("#name").focus();
+            return true;
+        }
+    };
+    const checkPhoneNumber = $("#phoneNumber").val();
+    const validCheckPhoneNumber = () => {
+        if (!checkPhoneNumber) {
+            $("#phoneNumber").val("");
+            $("#phoneNumber").focus();
+            return true;
+        }
+    };
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        if (validCheckName("")) {
+            setPopup({
+                open: true,
+                title: "아이디 찾기 실패ㅠㅠㅠㅠ",
+                message: "이름을 입력해주세요!!",
+            });
+            return;
+        }
+
+        if (validCheckPhoneNumber("")) {
+            setPopup({
+                open: true,
+                title: "아이디 찾기 실패ㅠㅠㅠㅠ",
+                message: "휴대폰 번호를 입력해주세요!!",
+            });
+            return;
+        }
+
+        if (!validation4()) {
+            setPopup({
+                open: true,
+                title: "아이디 찾기 실패ㅠㅠㅠㅠ",
+                message: "정보가 일치하지 않아요!!",
+            });
+            return;
+        } else {
+        }
+        setPopup({
+            open: true,
+            title: "아이디 찾기 성공!!",
+            message: "회원님의 아이디는 '0000' 입니다!!",
+            callback: function () {
+            },
+        });
+        if (validation4()) return;
+    };
+
+    return (
+        <>
+            <div className="search_signup">
+                <PopUp
+                    open={popup.open}
+                    setPopup={setPopup}
+                    message={popup.message}
+                    title={popup.title}
+                    callback={popup.callback}
+                />
+                <form onSubmit={onSubmitHandler}>
+                    <p className="search_upper_font"> 아이디 찾기 </p>
+                    <br/>
+                    <br/>
+                    <div className="blue-line-out">
+                        <div className="blue-line-in"></div>
+                    </div>
+                    <div className="yellow-line-out">
+                        <div className="yellow-line-in"></div>
+                    </div>
+                    <div>
+                        <label htmlFor="user-name" className="search_font">
+                            이름
+                        </label>
+                        <input
+                            className="search_inputs"
+                            id="name"
+                            name="user-name"
+                            type="text"
+                            placeholder="이름"
+                            value={name}
+                            onChange={onNameHandler}
+                        />{" "}
+                    </div>
+                    <div>
+                        <label className="search_font" htmlFor="user-phone">
+                            휴대폰 번호
+                        </label>
+                        <input
+                            className="search_inputs"
+                            id="phoneNumber"
+                            name="user-phone"
+                            type="text"
+                            placeholder="-를 제외하고 숫자만 입력하세요"
+                            value={phoneNumber}
+                            onChange={onChangePhoneNumber}
+                        />
+                        {errorPhoneNumber && (
+                            <div className="search_input_valid">숫자만 입력하세요</div>
+                        )}
+                    </div>
+                    <div>
+                        <input
+                            type="submit"
+                            className="search_button"
+                            value="아이디 찾기"
+                        />
+                    </div>
+                    <div className="search_search">
+                        <Link to="/login/PasswordSearch">비밀번호 찾기</Link> ||
+                        <Link to="/signup"> 회원가입 </Link>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
 }
 
 //
--- src/components/MyPage_SideMenubar.tsx
+++ src/components/MyPage_SideMenubar.tsx
@@ -1,96 +1,114 @@
 import MyPage_SideMenubarCss from '../css/MyPage_SideMenubarCss';
 import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
-import { BsPlusLg } from 'react-icons/bs';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import {BsPlusLg} from 'react-icons/bs';
+import {useState} from 'react';
+import "../css/main/animation.css";
 
-function isCeo(){
-    
-}
-
-function isAdmin(){
+function isCeo() {
 
 }
 
+function isAdmin() {
 
-function MyPage_SideMenubar(){
-    const [show,setShow]=useState(false);
-    const [show1,setShow1]=useState(false);
-    const [show2,setShow2]=useState(false);
+}
 
-    const handleshow=()=>{
-       setShow(!show)
-       if(!show){
-           setShow(true);
-       }else{
-           setShow(false);
-       }
+
+function MyPage_SideMenubar() {
+    const [show, setShow] = useState(false);
+    const [show1, setShow1] = useState(false);
+    const [show2, setShow2] = useState(false);
+
+    const handleshow = () => {
+        setShow(!show)
+        if (!show) {
+            setShow(true);
+        } else {
+            setShow(false);
+        }
     }
-    const handleshow1=()=>{
+    const handleshow1 = () => {
         setShow1(!show1)
-       if(!show1){
-           setShow1(true);
-       }else{
-           setShow1(false);
-       }
+        if (!show1) {
+            setShow1(true);
+        } else {
+            setShow1(false);
+        }
     }
-    const handleshow2=()=>{
+    const handleshow2 = () => {
         setShow2(!show2)
-       if(!show2){
-           setShow2(true);
-       }else{
-           setShow2(false);
-       }
+        if (!show2) {
+            setShow2(true);
+        } else {
+            setShow2(false);
+        }
     }
     return (
 
 
         <>
-                <MyPage_SideMenubarCss />
-                    <div className="sdBar">
-                        <Nav className="flex-column">
-                                    <Nav.Link style={{color:"black"}}>
-                                            <div style={{width:"100%",display:"flex", justifyContent:"space-between",padding:".5vw 0",margin:".2vw 0"}} onClick={handleshow}>
-                                                회원 관련<BsPlusLg onClick={handleshow}/>
-                                            </div>
-                                    </Nav.Link>
-                                    {show && <div className="membermenu">
-                                                <Nav.Link as={Link} className="text-dark" to="/member/">회원 정보</Nav.Link>
-                                                <Nav.Link as={Link} className="text-dark" to="/member/MyPage_MemberModify">회원 정보 수정</Nav.Link>
-                                                <Nav.Link as={Link} className="text-dark" to="/member/MyPage_SizeRegister">체형 등록</Nav.Link>
-                                                <Nav.Link as={Link} className="text-dark" to="/member/MyPage_MemberUnregister">회원 탈퇴</Nav.Link>    
-                                            </div>}
-                        </Nav>
+            <MyPage_SideMenubarCss/>
+            <div className="aa sdBar" style={{marginTop: "-250px"}}>
+                <Nav className="flex-column">
+                    <Nav.Link style={{color: "black"}}>
+                        <div style={{
+                            width: "100%",
+                            display: "flex",
+                            justifyContent: "space-between",
+                            padding: ".5vw 0",
+                            margin: ".2vw 0"
+                        }} onClick={handleshow}>
+                            회원 관련<BsPlusLg onClick={handleshow}/>
+                        </div>
+                    </Nav.Link>
+                    {show && <div className="membermenu">
+                        <Nav.Link as={Link} className="text-dark" to="/member/">회원 정보</Nav.Link>
+                        <Nav.Link as={Link} className="text-dark" to="/member/MyPage_MemberModify">회원 정보 수정</Nav.Link>
+                        <Nav.Link as={Link} className="text-dark" to="/member/MyPage_SizeRegister">체형 등록</Nav.Link>
+                        <Nav.Link as={Link} className="text-dark" to="/member/MyPage_MemberUnregister">회원 탈퇴</Nav.Link>
+                    </div>}
+                </Nav>
 
-                        <Nav className="flex-column">     
-                                    <Nav.Link style={{color:"black"}}>
-                                        <div style={{width:"100%",display:"flex", justifyContent:"space-between",padding:".5vw 0",margin:".2vw 0"}} onClick={handleshow1}>
-                                            사업자 관련 광고<BsPlusLg onClick={handleshow1}/>
-                                        </div>
-                                    </Nav.Link>
-                                    {show1 && <div className="ceomenu">
-                                            <Nav.Link as={Link} className="text-dark" to="/member/MyPage_Adapplication">광고 등록 신청</Nav.Link>
-                                            <Nav.Link as={Link} className="text-dark" to="/member/MyPage_ChartPage">게시물 차트 표시</Nav.Link>
-                                        </div>}
-                        </Nav>
+                <Nav className="flex-column">
+                    <Nav.Link style={{color: "black"}}>
+                        <div style={{
+                            width: "100%",
+                            display: "flex",
+                            justifyContent: "space-between",
+                            padding: ".5vw 0",
+                            margin: ".2vw 0"
+                        }} onClick={handleshow1}>
+                            사업자 관련 광고<BsPlusLg onClick={handleshow1}/>
+                        </div>
+                    </Nav.Link>
+                    {show1 && <div className="ceomenu">
+                        <Nav.Link as={Link} className="text-dark" to="/member/MyPage_Adapplication">광고 등록 신청</Nav.Link>
+                        <Nav.Link as={Link} className="text-dark" to="/member/MyPage_ChartPage">게시물 차트 표시</Nav.Link>
+                    </div>}
+                </Nav>
 
-                        <Nav className="flex-column">
-                            
-                                    <Nav.Link style={{color:"black"}}>
-                                        <div style={{width:"100%",display:"flex", justifyContent:"space-between",padding:".5vw 0",margin:".2vw 0"}} onClick={handleshow2}>
-                                            관리자 관련 광고<BsPlusLg onClick={handleshow2}/>
-                                        </div>
-                                    </Nav.Link>
-                                    {show2 && <div className="adminmenu">
-                                            <Nav.Link as={Link} className="text-dark" to="/member/MyPage_AdManagerPage">광고 관리</Nav.Link>
-                                            <Nav.Link as={Link} className="text-dark" to="/member/MyPage_AdProvePage">광고 등록 승인</Nav.Link>
-                                        </div>}
-                            
-                        </Nav>
-                        
-                    </div>
+                <Nav className="flex-column">
+
+                    <Nav.Link style={{color: "black"}}>
+                        <div style={{
+                            width: "100%",
+                            display: "flex",
+                            justifyContent: "space-between",
+                            padding: ".5vw 0",
+                            margin: ".2vw 0"
+                        }} onClick={handleshow2}>
+                            관리자 관련 광고<BsPlusLg onClick={handleshow2}/>
+                        </div>
+                    </Nav.Link>
+                    {show2 && <div className="adminmenu">
+                        <Nav.Link as={Link} className="text-dark" to="/member/MyPage_AdManagerPage">광고 관리</Nav.Link>
+                        <Nav.Link as={Link} className="text-dark" to="/member/MyPage_AdProvePage">광고 등록 승인</Nav.Link>
+                    </div>}
+
+                </Nav>
+
+            </div>
         </>
     );
 }
