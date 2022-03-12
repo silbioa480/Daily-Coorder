f51400b641664cbec2cc7375d8c9c95bec23ee09
jinnkimm7 <jinnkimm7@gmail.com>
2022-03-12 15:18:08 +0900

    진형: Navbar
    Navbar 반응형 구현

--- src/components/LoggedInNav.tsx
+++ src/components/LoggedInNav.tsx
@@ -8,30 +8,44 @@ import {
 } from "react-icons/bs";
 import { NavButton } from "../css/NavStyle";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function LoggedInNav() {
+  const [isOpen, setMenu] = useState(false);
+  const toggleMenu = () => {
+    setMenu(isOpen => !isOpen)
+  }
   return (
-    <ul className='login-list'>
-      <li className='logo-btn'>
-        <Link to="/upload"><BsPlusCircle /></Link>
-      </li>
+    <>
+      <ul className={isOpen ? "login-list" : "hidden-list"}>
+        <li className='logo-btn'>
+          <Link to="/upload"><BsPlusCircle /></Link>
+        </li>
 
-      <li className='logo-btn'>
-        <Link to="/feed"><BsBookmarkHeart /></Link>
-      </li>
+        <li className='logo-btn'>
+          <Link to="/feed"><BsBookmarkHeart /></Link>
+        </li>
 
-      <li className='logo-btn'>
-        <Link to="#"><BsBell /></Link>
-      </li>
+        <li className='logo-btn'>
+          <Link to="#"><BsBell /></Link>
+        </li>
 
-      <li className='logo-btn'>
-        <Link to="/member"><BsPerson /></Link>
-      </li>
+        <li className='logo-btn'>
+          <Link to="/member"><BsPerson /></Link>
+        </li>
 
-      <li className="logout-btn">
-        <Link to="">로그아웃</Link>
-      </li>
-    </ul>
+        <li className="logout-btn">
+          <Link to="">로그아웃</Link>
+        </li>
+      </ul>
+      {/* toggle button */}
+      <button
+        className='toggle-btn'
+        onClick={() => toggleMenu()}
+      >
+        <i className="fa-solid fa-bars"></i>
+      </button>
+    </>
   );
 }
 
--- src/views/Navigation.tsx
+++ src/views/Navigation.tsx
@@ -37,12 +37,6 @@ function Navigation() {
         {/* <NotLogInNav /> */}
         <LoggedInNav />
       </div>
-      {/* toggle button */}
-      <button
-        className='toggle-btn'
-      >
-        <i className="fa-solid fa-bars"></i>
-      </button>
     </Navbar >
   );
 }
