94ed633ee3bdd3c90eb9b31bb31e94163e4d3754
Blation <kisn3089@naver.com>
2022-03-13 21:19:22 +0900

    pull

--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -3,17 +3,31 @@ import Mainfollow from "../components/main/Mainfollow";
 import MainWeather from "../components/main/MainWeather";
 import MainAnother from "../components/main/MainAnother";
 import MainNewBrand from "../components/main/MainNewBrand";
+import {useParams} from "react-router-dom";
+
+
+interface IParams {
+    to_user_id: string;  //localhost/board/boardId 인데 url이라 첫타입은 스트링으로 가져와야함
+    from_user_id: string;
+}
+
+function followers() {
+    let params: IParams = useParams();
+    let to_user_id: string = params.to_user_id;
+    let from_user_id: string = params.from_user_id;
+}
+
 
 function Main() {
-  return (
-    <>
-      <Mainfollow />
-      <MainWeather />
-      <Banner />
-      <MainNewBrand />
-      <MainAnother />
-    </>
-  );
+    return (
+        <>
+            <Mainfollow/>
+            <MainWeather/>
+            <Banner/>
+            <MainNewBrand/>
+            <MainAnother/>
+        </>
+    );
 }
 
 export default Main;
