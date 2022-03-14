e08158b5946d3f9c315d1e711ebc9cf13e1d8cb0
Blation <kisn3089@naver.com>
2022-03-14 09:38:37 +0900

    Com

--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -3,19 +3,6 @@ import Mainfollow from "../components/main/Mainfollow";
 import MainWeather from "../components/main/MainWeather";
 import MainAnother from "../components/main/MainAnother";
 import MainNewBrand from "../components/main/MainNewBrand";
-import {useParams} from "react-router-dom";
-
-
-interface IParams {
-    to_user_id: string;
-    from_user_id: string;
-}
-
-function followers() {
-    let params: IParams = useParams();
-    let to_user_id: string = params.to_user_id;
-    let from_user_id: string = params.from_user_id;
-}
 
 
 function Main() {
