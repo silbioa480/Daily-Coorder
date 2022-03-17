491df26f1ca1e32c137437dfc25d1a92a11bacb8
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-03-17 15:51:13 +0900

    광고관련부분 주석처리

--- src/routes/MyPage.tsx
+++ src/routes/MyPage.tsx
@@ -5,10 +5,10 @@ import MyPage_SizeRegister from "../components/MyPage_SizeRegister";
 import MyPage_MemberUnregister from "../components/MyPage_MemberUnregister";
 import MyPage_MemberModify from "../components/MyPage_MemberModify";
 import MyPage_MemberInformation from "../components/MyPage_MemberInformation";
-import MyPage_AdProvePage from "../components/MyPage_AdProvePage";
+// import MyPage_AdProvePage from "../components/MyPage_AdProvePage";
 import MyPage_ChartPage from "../components/MyPage_ChartPage";
-import MyPage_AdManagerPage from "../components/MyPage_AdManagerPage";
-import MyPage_Adapplication from "../components/MyPage_Adapplication";
+// import MyPage_AdManagerPage from "../components/MyPage_AdManagerPage";
+// import MyPage_Adapplication from "../components/MyPage_Adapplication";
 import MyPage_detailChartPage from "../components/MyPage_detailChartPage";
 
 
@@ -32,18 +32,18 @@ function MyPage() {
           <Route exact path="/member/MyPage_SizeRegister">
             <MyPage_SizeRegister />
           </Route>
-          <Route exact path="/member/MyPage_Adapplication">
+          {/* <Route exact path="/member/MyPage_Adapplication">
             <MyPage_Adapplication />
-          </Route>
+          </Route> */}
           <Route exact path="/member/MyPage_ChartPage">
             <MyPage_ChartPage />
           </Route>
-          <Route exact path="/member/MyPage_AdProvePage">
+          {/* <Route exact path="/member/MyPage_AdProvePage">
             <MyPage_AdProvePage />
           </Route>
           <Route path="/member/MyPage_AdManagerPage">
             <MyPage_AdManagerPage />
-          </Route>
+          </Route> */}
           <Route path="/member/MyPage_ChartPage/MyPage_detailChartPage">
             <MyPage_detailChartPage />
           </Route>
