bfcce952cb553839e83bea16fb6685043337473d
silbioa480 <silbioa480@gmail.com>
2022-03-17 13:32:03 +0900

    종호: modified Navigation by isLogin

--- src/views/Navigation.tsx
+++ src/views/Navigation.tsx
@@ -1,13 +1,14 @@
-import { Nav, Navbar } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { BsSearch } from "react-icons/bs";
-import NotLogInNav from "../components/NotLogInNav";
 import LoggedInNav from "../components/LoggedInNav";
 
 import "../css/main/animation.css";
 import "../css/Navi.css";
-import { useState } from 'react';
+import { useRecoilValue } from "recoil";
+import { isLoginAtom } from "../atom";
+import NotLogInNav from "../components/NotLogInNav";
 
 const NavLine = styled.div`
   display: flex;
@@ -16,32 +17,29 @@ const NavLine = styled.div`
 `;
 
 function Navigation() {
+  const isLogin = useRecoilValue(isLoginAtom);
+
   return (
     <>
-      <Navbar id='navbar'>
+      <Navbar id="navbar">
         <div className="navbar-box">
-          <div className='navbar-right'>
+          <div className="navbar-right">
             <h1 className="title">
-              <Link to="/" className="title">Daliy Coorder</Link>
+              <Link to="/" className="title">
+                Daliy Coorder
+              </Link>
             </h1>
             <form className="search-form">
-              <input
-                type="text"
-                name="search"
-                id="search"
-                placeholder="검색"
-              />
-              <BsSearch className='search-icon'
-              />
+              <input type="text" name="search" id="search" placeholder="검색" />
+              <BsSearch className="search-icon" />
             </form>
           </div>
-          {/* <NotLogInNav /> */}
-          <LoggedInNav />
+          {isLogin ? <LoggedInNav /> : <NotLogInNav />}
         </div>
-      </Navbar >
-      <ul className='ukraine'>
-        <li className='blue'></li>
-        <li className='yellow'></li>
+      </Navbar>
+      <ul className="ukraine">
+        <li className="blue"></li>
+        <li className="yellow"></li>
       </ul>
     </>
   );
