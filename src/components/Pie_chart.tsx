e393358fb581640b94ef9dcc0eb997249d249599
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-03-18 09:11:24 +0900

    그래프주석처리

--- src/components/Pie_chart.tsx
+++ src/components/Pie_chart.tsx
@@ -1,47 +1,48 @@
-import React, { ComponentLifecycle } from "react";
-import ReactApexChart from "react-apexcharts";
+// import React, { ComponentLifecycle } from "react";
+// import ReactApexChart from "react-apexcharts";
 
-interface Component <P={},S={}> extends ComponentLifecycle <P,S>{}
+// interface Component <P={},S={}> extends ComponentLifecycle <P,S>{}
 
-class Pie_chart extends React.Component<{},{options:object , series:Array<Object>}> {
+// class Pie_chart extends React.Component<{},{options:object , series:Array<Object>}> {
     
 
-    constructor(props : object){
-      super(props);
-      this.state={
-          series:[44,55,13,24,42],
-          options:{
-            chart:{
-              width:"380px",
-              type:"pie"
-            },
-            labels:["Like","Age","Man","Woman","Reply"],
-            responsive:[{
-              breakpoint:"480",
-              options:{
-                chart:{
-                  width:"800px",
-                },
-                legend:{
-                  position:"right"
-                }
-              }
-            }]
-          }
-      };
-    }
+//     constructor(props : object){
+//       super(props);
+//       this.state={
+//           series:[44,55,13,24,42],
+//           options:{
+//             chart:{
+//               width:"380px",
+//               type:"pie"
+//             },
+//             labels:["Like","Age","Man","Woman","Reply"],
+//             responsive:[{
+//               breakpoint:"480",
+//               options:{
+//                 chart:{
+//                   width:"800px",
+//                 },
+//                 legend:{
+//                   position:"right"
+//                 }
+//               }
+//             }]
+//           }
+//       };
+//     }
 
-  render() {
-    return (
+//   render() {
+//     return (
      
-          <>
-            <div style={{display:"flex",justifyContent:"flex-end",marginTop:"3vw"}}>
-                <ReactApexChart options={this.state.options}  series={this.state.series} type="pie" width={700}/>
-            </div>
-          </>
+//           <>
+//             <div style={{display:"flex",justifyContent:"flex-end",marginTop:"3vw"}}>
+//                 <ReactApexChart options={this.state.options}  series={this.state.series} type="pie" width={700}/>
+//             </div>
+//           </>
       
-    );
-  }
-}
+//     );
+//   }
+// }
 
-export default Pie_chart;
+// export default Pie_chart;
+export {}
