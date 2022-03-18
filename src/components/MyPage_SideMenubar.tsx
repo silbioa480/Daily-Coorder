714cea6252db9213b0f9e72725c109ee59985cfb
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-03-18 16:59:27 +0900

    사이드메뉴바수정

--- src/components/MyPage_SideMenubar.tsx
+++ src/components/MyPage_SideMenubar.tsx
@@ -19,28 +19,34 @@ function MyPage_SideMenubar() {
     const [ceo, setCeo] = useState(false);
 
   
-        if(isLogin !== false){
-            if(memberId.is_business === true){
-                setUser(true);
-                setCeo(true);
-            }else{
-                setUser(true);
-                setCeo(false);
-            }
-        }else if(isLogin === false){
-            <Modal>
-                    <Modal.Header closeButton>
-                    <Modal.Title>로그인 오류</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>로그인 먼저 하세요</Modal.Body>
-                    <Modal.Footer>
-                    <Button variant="primary">
-                        <Link to="/login">로그인 하기</Link>
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        }
+    // function userOrBusiness(){
+    //     if(isLogin !== false){
+    //         if(memberId.is_business === true){
+    //             setUser(true);
+    //             setCeo(true);
+    //         }else{
+    //             setUser(true);
+    //             setCeo(false);
+    //         }
+    //     }else if(isLogin === false){
+    //         <Modal>
+    //                 <Modal.Header closeButton>
+    //                 <Modal.Title>로그인 오류</Modal.Title>
+    //                 </Modal.Header>
+    //                 <Modal.Body>로그인 먼저 하세요</Modal.Body>
+    //                 <Modal.Footer>
+    //                 <Button variant="primary">
+    //                     <Link to="/login">로그인 하기</Link>
+    //                 </Button>
+    //             </Modal.Footer>
+    //         </Modal>
+    //     }
+    // }
     
+    // useEffect(()=>{
+    //     userOrBusiness();
+    // },[]);
+
     return (
 
 
@@ -62,12 +68,12 @@ function MyPage_SideMenubar() {
                     <div className="membermenu">
                         <Nav.Link as={Link} className="text-dark" to="/member/">회원 정보</Nav.Link>
                         <Nav.Link as={Link} className="text-dark" to="/member/MyPage_MemberModify">회원 정보 수정</Nav.Link>
-                        <Nav.Link as={Link} className="text-dark" to="/member/MyPage_SizeRegister">체형 등록</Nav.Link>
+                        <Nav.Link as={Link} className="text-dark" to="/member/MyPage_SizeRegister">체형 수정</Nav.Link>
                         <Nav.Link as={Link} className="text-dark" to="/member/MyPage_MemberUnregister">회원 탈퇴</Nav.Link>
                     </div>
                 </Nav>}
 
-                {ceo && <Nav className="flex-column">
+                {/* {ceo && <Nav className="flex-column">
                     <Nav.Link style={{color: "black"}}>
                         <div style={{
                             width: "100%",
@@ -80,10 +86,10 @@ function MyPage_SideMenubar() {
                         </div>
                     </Nav.Link>
                      <div className="ceomenu">
-                        {/* <Nav.Link as={Link} className="text-dark" to="/member/MyPage_Adapplication">광고 등록 신청</Nav.Link> */}
+                        <Nav.Link as={Link} className="text-dark" to="/member/MyPage_Adapplication">광고 등록 신청</Nav.Link>
                         <Nav.Link as={Link} className="text-dark" to="/member/MyPage_ChartPage">내가 올린 게시물 보기</Nav.Link>
                     </div>
-                </Nav>}
+                </Nav>} */}
 
                 {/* {admin && <Nav className="flex-column">
 
