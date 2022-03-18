1acc8137948c5182fbb7b5b0aef8778fc0d113fb
Blation <kisn3089@naver.com>
2022-03-18 16:58:24 +0900

    eeq

--- src/components/main/Banner.tsx
+++ src/components/main/Banner.tsx
@@ -1,56 +1,54 @@
 // 부트 스트랩 Carousel
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Carousel} from "react-bootstrap";
 import React from "react";
-import {Link} from "react-router-dom";
 
 import "../../css/main/Banner.css";
 import "../../css/main/animation.css";
-import mainBanners5 from "../../img/main/mainBanners5.png";
-import mainBanner2 from "../../img/main/mainBanner2.png";
-import mainBanner3 from "../../img/main/mainBanner3.png";
 
+import mainBanners5 from "../../img/main/mainBanners5.png";
 
 function Banner() {
     return (
         <>
             <div className="square-wrapper">
-                <div className="square"></div>
+                <div className="square">
+                    <img className="squarepic" src={mainBanners5}></img>
+                </div>
             </div>
 
-            <div className="ww banner-out">
-                <Carousel fade className="carousel-dark BannerCar">
-                    <Carousel.Item className="bannerpic">
-                        <Link to="/signup">
-                            <img
-                                className="d-block w-100"
-                                src={mainBanners5}
-                                alt="First slide"
-                            />
-                        </Link>
-                    </Carousel.Item>
+            {/*<div className="ww banner-out">*/}
+            {/*    <Carousel fade className="carousel-dark BannerCar">*/}
+            {/*        <Carousel.Item className="bannerpic">*/}
+            {/*            <Link to="/signup">*/}
+            {/*                <img*/}
+            {/*                    className="d-block w-100"*/}
+            {/*                    src={mainBanner1}*/}
+            {/*                    alt="First slide"*/}
+            {/*                />*/}
+            {/*            </Link>*/}
+            {/*        </Carousel.Item>*/}
 
-                    <Carousel.Item className="bannerpic">
-                        <Link to="/signup">
-                            <img
-                                className="d-block w-100"
-                                src={mainBanner2}
-                                alt="Second slide"
-                            />
-                        </Link>
-                    </Carousel.Item>
+            {/*        <Carousel.Item className="bannerpic">*/}
+            {/*            <Link to="/signup">*/}
+            {/*                <img*/}
+            {/*                    className="d-block w-100"*/}
+            {/*                    src={mainBanner2}*/}
+            {/*                    alt="Second slide"*/}
+            {/*                />*/}
+            {/*            </Link>*/}
+            {/*        </Carousel.Item>*/}
 
-                    <Carousel.Item className="bannerpic">
-                        <Link to="/signup">
-                            <img
-                                className="d-block w-100"
-                                src={mainBanner3}
-                                alt="Second slide"
-                            />
-                        </Link>
-                    </Carousel.Item>
-                </Carousel>
-            </div>
+            {/*        <Carousel.Item className="bannerpic">*/}
+            {/*            <Link to="/signup">*/}
+            {/*                <img*/}
+            {/*                    className="d-block w-100"*/}
+            {/*                    src={mainBanner3}*/}
+            {/*                    alt="Second slide"*/}
+            {/*                />*/}
+            {/*            </Link>*/}
+            {/*        </Carousel.Item>*/}
+            {/*    </Carousel>*/}
+            {/*</div>*/}
             <div style={{width: "100%", height: "30px"}}></div>
         </>
     );
